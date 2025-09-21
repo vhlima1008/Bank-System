dff18bbc6ac010e0733cc73562adcd7e6940007a
Victor Hugo <vhlima1008@gmail.com>
2025-09-21 19:47:26 -0300

    feat: added the mainFrame screen with deposit button

--- main.py
+++ main.py
@@ -12,52 +12,162 @@ class LoginApp(ctk.CTk):
         self.geometry("500x350")
 
         # Entry Var
-        self.username_var = ctk.StringVar()
-        self.password_var = ctk.StringVar()
+        self.usernameVar = ctk.StringVar()
+        self.passwordVar = ctk.StringVar()
+        self.depositVar = ctk.DoubleVar(value=0.0)
+        self.account = 0
+        self.extract = []
 
         # Component Creation
-        self.create_widgets()
+        self.loginFrame = None
+        self.mainFrame = None
+        self.depositFrame = None
+        self.createLoginFrame()
 
-    def create_widgets(self):
-        frame = ctk.CTkFrame(master=self)
-        frame.pack(pady=20, padx=60, fill="both", expand=True)
+    # Login Screen -------------------------------------------------------------
+    def createLoginFrame(self):
+        self.loginFrame = ctk.CTkFrame(master=self)
+        self.loginFrame.pack(pady=20, padx=60, fill="both", expand=True)
 
-        label = ctk.CTkLabel(master=frame, text="Login System")
-        label.pack(pady=12, padx=10)
+        loginLabel = ctk.CTkLabel(master=self.loginFrame, text="Login System")
+        loginLabel.pack(pady=12, padx=10)
 
         # User Input
-        name_entry = ctk.CTkEntry(
-            master=frame,
+        usernameEntry = ctk.CTkEntry(
+            master=self.loginFrame,
             placeholder_text="Username",
-            textvariable=self.username_var
+            textvariable=self.usernameVar
         )
-        name_entry.pack(pady=12, padx=10)
+        usernameEntry.pack(pady=12, padx=10)
 
         # Password Input
-        password_entry = ctk.CTkEntry(
-            master=frame,
+        passwordEntry = ctk.CTkEntry(
+            master=self.loginFrame,
             placeholder_text="Password",
             show="*",
-            textvariable=self.password_var
+            textvariable=self.passwordVar
         )
-        password_entry.pack(pady=12, padx=10)
+        passwordEntry.pack(pady=12, padx=10)
 
         # login Button
-        login_button = ctk.CTkButton(
-            master=frame,
+        loginButton = ctk.CTkButton(
+            master=self.loginFrame,
             text="Login",
             command=self.login
         )
-        login_button.pack(pady=12, padx=10)
+        loginButton.pack(pady=12, padx=10)
 
         # Checkbox
-        checkbox = ctk.CTkCheckBox(master=frame, text="Remember Me")
-        checkbox.pack(pady=12, padx=10)
+        loginCheckbox = ctk.CTkCheckBox(master=self.loginFrame, text="Remember Me")
+        loginCheckbox.pack(pady=12, padx=10)
 
+    # Main Screen -------------------------------------------------------------
+    def createMainFrame(self, username):
+        self.mainFrame = ctk.CTkFrame(master=self)
+        self.mainFrame.pack(pady=20, padx=60, fill="both", expand=True)
+
+        mainLabel = ctk.CTkLabel(master=self.mainFrame, text=f"{username}' Account.")
+        mainLabel.pack(pady=12, padx=10)
+
+        accountLabel = ctk.CTkLabel(
+            master=self.mainFrame,
+            text=f"Account: {self.account}",
+            font=("Arial", 20)
+        )
+        accountLabel.pack(pady=20)
+
+        depositButton = ctk.CTkButton(
+            master=self.mainFrame,
+            text="Deposit a value",
+            command=self.openDepositScreen
+        )
+        depositButton.pack(pady=10)
+
+
+        logoutButton = ctk.CTkButton(
+            master=self.mainFrame,
+            text="Logout",
+            command=self.logout
+        )
+        logoutButton.pack(pady=10)
+    
+    # Deposit Screen -------------------------------------------------------------
+    def createDepositFrame(self):
+        self.depositFrame = ctk.CTkFrame(master=self)
+        self.depositFrame.pack(pady=20, padx=60, fill="both", expand=True)
+
+        depositLabel = ctk.CTkLabel(master=self.depositFrame, text="Deposit")
+        depositLabel.pack(pady=12, padx=10)
+
+        # Deposit Input
+        depositEntry = ctk.CTkEntry(
+            master=self.depositFrame,
+            placeholder_text="$100,00",
+            textvariable=self.depositVar
+        )
+        depositEntry.pack(pady=12, padx=10)
+
+        # Confirm Button
+        confirmButton = ctk.CTkButton(
+            master=self.depositFrame,
+            text="Deposit it",
+            command=self.depositValue
+        )
+        confirmButton.pack(pady=12, padx=10)
+
+        # Close Button
+        closeButton = ctk.CTkButton(
+            master=self.depositFrame,
+            text="Close",
+            command=self.closeDepositScreen
+        )
+        closeButton.pack(pady=12, padx=10)
+
+    # Methods ------------------------------------------------------------------
     def login(self):
-        password_hashed = hash(self.password_var.get())
-        print("Username:", self.username_var.get())
-        print("Password Hash:", password_hashed)
+        passwordHashed = hash(self.passwordVar.get())
+        username = self.usernameVar.get()
+
+        print("Username:", username)
+        print("Password Hash:", passwordHashed)
+
+        if self.loginFrame is not None:
+            self.loginFrame.destroy()
+
+        self.createMainFrame(username)
+    
+
+    def logout(self):
+        if self.mainFrame is not None:
+            self.mainFrame.destroy()
+
+        self.usernameVar = ctk.StringVar()
+        self.passwordVar = ctk.StringVar()
+
+        self.createLoginFrame()
+
+    def openDepositScreen(self):
+        if self.mainFrame is not None:
+            self.mainFrame.destroy()
+
+        self.createDepositFrame()
+
+    def closeDepositScreen(self):
+        if self.depositFrame is not None:
+            self.depositFrame.destroy()
+        username = self.usernameVar.get()
+        
+        self.createMainFrame(username)
+
+    def depositValue(self):
+        value = float(self.depositVar.get() or 0)  # get the value
+        self.account += value
+        self.extract.append(f"Deposited: {value}")
+
+        # after deposited, come to main
+        if self.depositFrame is not None:
+            self.depositFrame.destroy()
+        self.createMainFrame(self.usernameVar.get())
 
 
 if __name__ == "__main__":
