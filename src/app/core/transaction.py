f2636e85ddeb380207dfb43cc028e9062480d4ee
Victor Hugo <vhlima1008@gmail.com>
2025-09-28 16:02:38 -0300

    feat: a class to control every movement.
    ## Previous Problem
    > Could't control all code duplicates.
    So much error.
    
    ## Solution
    > Centralize all comands and dont have havy duplication.

---
+++ src/app/core/transaction.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+from .user import User # WHEN TESTING HERE USE: user (JUST) | REMEMBER TO PUT .user AFTER END TESTS
+from .financing import Financing # WHEN TESTING HERE USE: financing (JUST) | REMEMBER TO PUT .financing AFTER END TESTS
+from .extract import Extract# WHEN TESTING HERE USE: extract (JUST) | REMEMBER TO PUT .extract AFTER END TESTS
+
+# Need to add an option to select the day for paying (or the current for default)
+
+DATE_FMT = "%Y-%m-%d %H:%M:%S"
+
+class Transaction:
+    def __init__(self, client:User):
+        self.client = client 
+        self.financing = Financing()
+        self.extract = Extract()
+        self.status = ""
+
+    def _now(self) -> str: 
+        return datetime.now().strftime(DATE_FMT)
+    
+    def execute(self, kind:str, amount:float, nParcel:int=1):
+        self.status = "refused"
+        if amount > 0:
+            if kind == "deposit":
+                self.status = "aproved"
+                self.client.deposit(amount)
+            
+            elif kind == "withdraw":
+                if self.client.balance >= amount:
+                    self.status = "aproved"
+                    self.client.withdraw(amount)
+
+            elif kind == "financing":
+                if amount > 0 and nParcel > 0:
+                    self.status = "aproved"
+                    self.financing.simulation(amount, nParcel)
+            
+            self.extract.generate(self.status, self._now(), kind, amount, self.client.balance)
+        else:
+            self.extract.generate(self.status, self._now(), kind, amount, self.client.balance)
